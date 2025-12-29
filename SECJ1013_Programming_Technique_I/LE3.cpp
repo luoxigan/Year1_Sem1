2d7b485f40c1361df30abc8c8da36305a2bafe38
GAN LUO XI <lxgan26@gmail.com>
2025-12-29 22:02:08 +0800

    Add files via upload

---
+++ SECJ1013_Programming_Technique_I/LE3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int num1 ;
+  int num2 ;
+  cin >> num1 >> num2 ;
+  
+  if (num1<num2)
+  {
+      int i = num1;
+      if (i%2==0) 
+	  	i++;
+		   
+      do
+      {
+      	cout << i << " ";
+      	i+=2;
+	  } while (i <= num2);
+}
+  
+  else 
+  {
+      int i =num1;
+      if (i%2!=0) 
+	  	i--;
+      
+      	do{      
+		  cout << i << " ";    
+          i -= 2;  
+        }while (i>=num2);  
+  }
+    return 0;
+}
